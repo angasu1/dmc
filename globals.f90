baade02bf41621ae0e6d72078dfc096f4c7dbe76
angasu <angasu@gmail.com>
2018-07-24 18:11:46 -0500

    some cleaning

--- globals.f90
+++ globals.f90
@@ -37,7 +37,7 @@ Module global_variables
   real(rk)::rfb,rfbmax,rfbmin,drfb,dtau,rmax,rmin,umb_ang,xn(3),initd=0.0_rk
   real(rk)::diffcHe,diffchered,diffccm,A,B,C,dhe,mHe,dmassred,mtot
   real(rk)::factorrotx=1.0_rk,factorroty=1.0_rk,factorrotz=1.0_rk
-  integer(ik)::nw,state,node,nstps,nhe,nmon=1,nruns,norg,lmax,nmesh,istat
+  integer(ik):: nw,state,node,nstps,nhe,nmon=1,nruns,norg,lmax,nmesh,istat
   integer(ik)::idum,jtot,npoin,nrad,counter=0
   character (len=80)::molname
 
