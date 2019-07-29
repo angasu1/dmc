38c36c41c685608ec0318ffa0ca33fedadfa8e6a
Antonio Gamboa Suarez <angasu@gmail.com>
2019-07-29 16:20:21 -0500

    optimizacion de la subrutina interpol

--- potinterpol.f90
+++ potinterpol.f90
@@ -12,13 +12,14 @@ module potinter
        real(rk),intent(out)            :: Vsum
        real(rk),intent(in)             :: r,cthet
        integer(ik)                     :: i, j
-       real(rk)                        :: th1, th2, r1, r2 !, th0, r0, vsum
+       real(rk)                        :: cth1, cth2, r1, r2 !, th0, r0, vsum
        integer(ik)                     :: linear, lineath
-       real(rk), allocatable           :: pl(:), vl(:)
+      !real(rk), allocatable           :: pl(:), vl(:)
+       real(rk)                        :: pl, vl,slopeth,sloper
        
        nColumna = ntermspot + 2
-       allocate(pl(nColumna-1))                
-       allocate(vl(nColumna-1))                
+      !allocate(pl(nColumna-1))                
+      !allocate(vl(nColumna-1))                
                                  
        !interpolación, encontrar los vlam, plam y sacar el potencial, para nuevos r0 y th0
        linear = 1 + int((1000._rk*(r-2._rk))/33._rk)
@@ -26,26 +27,30 @@ module potinter
        lineath = 1 - nint(500._rk*(cthet-1._rk)) 
        
 
-       th1 = plam(lineath,1)                
-       th2 = plam(lineath+1,1)              
+       cth1 = plam(lineath,1)                
+       cth2 = plam(lineath+1,1)              
        r1 = vlam(linear,1)                  
        r2 = vlam(linear+1,1)                
 
        ! Asegura estar en la linea correcta 
-       if(abs(cthet-th1)>(1._rk/1000._rk).or.abs(r-r1)>(33._rk/1000._rk))then
+       if(abs(cthet-cth1)>(1._rk/1000._rk).or.abs(r-r1)>(33._rk/1000._rk))then
            write(*,*) "no estas en la línea correcta"
            stop                                 
        end if         
 
+       if (lineath+1.gt.1001) stop 'fuera de rango theta'
+       if (linear+1.gt.1001) stop 'fuera de rango r'
+
        ! encuentra valores nuevos de vlam y plam
+       vsum=0
+       slopeth=(cthet-cth1)/(cth2-cth1)
+       sloper=(r-r1)/(r2-r1)
        do j = 2, nColumna                   
-           pl(j-1) = plam(lineath,j) + ((cthet-th1)/(th2-th1))*(plam(lineath+1,j) - plam(lineath,j))
+           pl = plam(lineath,j) + slopeth*(plam(lineath+1,j) - plam(lineath,j))
+           vl = vlam(linear,j)  + sloper*(vlam(linear+1,j) - vlam(linear,j))
+           vsum=vsum+pl*vl
        end do !j = 2, nColumna              
                                         
-       do j = 2, nColumna                   
-           vl(j-1) = vlam(linear,j) + ((r-r1)/(r2-r1))*(vlam(linear+1,j) - vlam(linear,j))
-       end do !j = 2, nColumna 
-       vsum = sum(vl*pl)                
                                         
                                         
    10 format(*(f28.16,2x))               
